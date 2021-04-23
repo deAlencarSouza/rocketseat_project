7f31eb1c681786afeff5e06b460ca02c91530b96
Fernando de Alencar Souza <77296410+deAlencarSouza@users.noreply.github.com>
2021-04-23 10:49:17 -0300

    master

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,7 +1,12 @@
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/appbar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,41 +14,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.getUser();
+    controller.getQuizzers();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LevelButtonWidget(label: 'Fácil'),
-                LevelButtonWidget(label: 'Médio'),
-                LevelButtonWidget(label: 'Difícil'),
-                LevelButtonWidget(label: 'Perito'),
-              ],
-            ),
-            SizedBox(height: 24),
-            Expanded(
-              child: GridView.count(
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                crossAxisCount: 2,
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(
+          user: controller.user!,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget(),
+                  LevelButtonWidget(label: 'Fácil'),
+                  LevelButtonWidget(label: 'Médio'),
+                  LevelButtonWidget(label: 'Difícil'),
+                  LevelButtonWidget(label: 'Perito'),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 24),
+              Expanded(
+                child: GridView.count(
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  crossAxisCount: 2,
+                  children: controller.quizzers!
+                      .map((e) => QuizCardWidget(
+                            title: e.title,
+                            percent: e.questionAnswered / e.questions.length,
+                            completed:
+                                '${e.questionAnswered}/${e.questions.length}',
+                          ))
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
   }
 }
--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -5,6 +5,15 @@ import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
+  final String title;
+  final String completed;
+  final double percent;
+  const QuizCardWidget(
+      {Key? key,
+      required this.title,
+      required this.completed,
+      required this.percent})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,7 +36,7 @@ class QuizCardWidget extends StatelessWidget {
           ),
           SizedBox(height: 20),
           Text(
-            'Gerenciamento de Estado',
+            title,
             style: AppTextStyles.heading15,
           ),
           SizedBox(height: 20),
@@ -36,13 +45,15 @@ class QuizCardWidget extends StatelessWidget {
               Expanded(
                 flex: 1,
                 child: Text(
-                  '3/10',
+                  completed,
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
                 flex: 4,
-                child: ProgressIndicatorWidget(value: 0.3),
+                child: ProgressIndicatorWidget(
+                  value: percent,
+                ),
               ),
             ],
           ),
